6e4d3b0258f3f71aab22d7d6eb1f7fb2a9e95435
jaerius <rylynn1029@naver.com>
2024-03-29 20:46:30 +0900

    backgrouncolor changed

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sidebar from "../../Layout/Sidebar"
 import Header from "../../Layout/Header"
+import './LoginPage.css'
 
 function LoginPage() {
     
@@ -8,9 +9,10 @@ function LoginPage() {
           <div className="LoginPage">
             <div className="content-wrapper">
                 <Header/>
-                <h1>Connect your wallet!</h1>
-                <button className="button1">connect wallet</button>
-                
+                <div className="content">
+                  <div className="name">FAVPICK</div>
+                    <button className="button1">connect wallet</button>
+                </div>
 
             </div>
           </div>
