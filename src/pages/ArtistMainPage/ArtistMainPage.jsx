7593f1b4987cc117700a2a4ac62b25c06b2d9f5a
jaerius <rylynn1029@naver.com>
2024-03-30 15:05:04 +0900

    아티스트 메인 페이지 대충 완료

--- src/pages/ArtistMainPage/ArtistMainPage.jsx
+++ src/pages/ArtistMainPage/ArtistMainPage.jsx
@@ -1,10 +1,20 @@
 import './ArtistMainPage.css'
 import { GoBack, BottomSidebar, ContentWrapper, LogoAndHead } from '../../Components';
 import React from 'react';
+import Sidebar from '../../Layout/Sidebar';
 
-const idolData =[
-    { id: 1, Name: "HUH YUNJIN", agency: "LE SSERAFIM" ,Img: "img", vote: "2.93k"},
-]
+const idolsData = [
+    { id: 1, name: "Kim Chewon", agency: "LE SSERAFIM", profileImg: "", vote:2.97 },
+    { id: 2, name: "Park Jimin", agency: "BTS", profileImg: "" },
+    { id: 3, name: "Kang Daniel", agency: "KONNECT Entertainment", profileImg: "" },
+    { id: 4, name: "Lee Chaeyeon", agency: "WM Entertainment", profileImg: "" },
+    { id: 5, name: "Miyawaki Sakura", agency: "HKT48", profileImg: "" },
+    { id: 6, name: "Choi Yena", agency: "Yuehua Entertainment", profileImg: "" },
+    { id: 7, name: "Kwon Eunbi", agency: "Woollim Entertainment", profileImg: "" },
+    { id: 8, name: "Kim Minju", agency: "Urban Works Media", profileImg: "" },
+    { id: 9, name: "Ahn Yujin", agency: "Starship Entertainment", profileImg: "" },
+    { id: 10, name: "Jang Wonyoung", agency: "Starship Entertainment", profileImg: "" }
+  ];
 
 function ArtistMainPage() {
 
@@ -13,30 +23,52 @@ function ArtistMainPage() {
     <ContentWrapper>
         <div className="ArtistMainPage">
             <div className='ArtistMainPageidolImg'>
-                {idolData[0].Img}
+                {idolsData[0].Img}
             </div>
             <div className='content-wrapper'>
             <div className='ArtistMainPageGlass'>
                 <div className='ArtistMainPageidolname'>
-                    {idolData[0].Name}
+                    {idolsData[0].name}
                 </div>
                 <div className='ArtistMainPageidolagency'>
-                    {idolData[0].agency}
+                    {idolsData[0].agency}
                 </div>
                 <div className='TotalVotedTitle'>
-                    Total Voted
+                    Total Voted Rappo
                 </div>
                 <div className='TotalVote'>
-                    {idolData[0].vote}
+                    {idolsData[0].vote}
                 </div>
                 <button className='VoteButton'>
                     Vote
                 </button>
             </div>
             <div className="ArtistMainPageContent">
+
+                <div className="RappoTitle">Rappo Ranking</div>
+
+                    <div className="RappoRanking">    
+                    {idolsData.map(idolsData => (
+                    <div key={idolsData.id} className="Artistcontents1">
+                        <div className="ProfileImg">
+                        {idolsData.profileImg} {/* 이 부분에 실제 이미지 태그나 경로를 사용할 수 있습니다. */}
+                        </div>
+                        <div className="info">
+                        <div className="name">
+                            {idolsData.name}
+                        </div>
+                        <div className="agency">
+                            {idolsData.agency}
+                        </div>
+                        </div>
+                    </div>
+                    ))}
+                    </div>
             </div>
             </div>
         </div>
+        
+        <Sidebar/>
     </ContentWrapper>
     )
 }
