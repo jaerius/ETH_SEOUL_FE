df14374bacf7c7baf0e713aa5911f9629f3009ab
jaerius <rylynn1029@naver.com>
2024-03-30 22:25:16 +0900

    commit agian

--- src/pages/CommunityPage/CommunityDetail.jsx
+++ src/pages/CommunityPage/CommunityDetail.jsx
@@ -7,9 +7,18 @@ import Sidebar from '../../Layout/Sidebar';
 import './CommunityDetail.css';
 import { useNavigate } from 'react-router-dom';
 
-const idolsData = [
-  { id: 1, name: "Kim Chewon", agency: "LE SSERAFIM", profileImg: "../../Image/BackIcon.png", vote:2.97 },
-]
+const idol = [
+  { id: 1, name: "Kim Chewon", agency: "LE SSERAFIM", profileImg: "", content:"hello it's me mario" },
+  { id: 2, name: "Park Jimin", agency: "BTS", profileImg: "" },
+  { id: 3, name: "Kang Daniel", agency: "KONNECT Entertainment", profileImg: "" },
+  { id: 4, name: "Lee Chaeyeon", agency: "WM Entertainment", profileImg: "" },
+  { id: 5, name: "Miyawaki Sakura", agency: "HKT48", profileImg: "" },
+  { id: 6, name: "Choi Yena", agency: "Yuehua Entertainment", profileImg: "" },
+  { id: 7, name: "Kwon Eunbi", agency: "Woollim Entertainment", profileImg: "" },
+  { id: 8, name: "Kim Minju", agency: "Urban Works Media", profileImg: "" },
+  { id: 9, name: "Ahn Yujin", agency: "Starship Entertainment", profileImg: "" },
+  { id: 10, name: "Jang Wonyoung", agency: "Starship Entertainment", profileImg: "" }
+];
 
 const CommunityDetail = () => {
   const { id } = useParams(); // URL에서 id 파라미터를 가져옴
@@ -43,8 +52,26 @@ const CommunityDetail = () => {
         <div className='CommunityName'>
           CommunityName
         </div>
+        <div className='CommunityTab'>
+  {idol.map(idol => (
+    <div key={idol.id} className="Artistcontents">
+      <div className="ProfileImg">
+        {/* ... */}
+      </div>
+      <div className="info">
+        <div className="nameAgencyWrapper">
+          <div className="name">{idol.name}</div>
+          <div className="agency">{idol.agency}</div>
+        </div>
+        {idol.content && (
+          <div className="content">{idol.content}</div>
+        )}
+      </div>
+    </div>
+  ))}
+</div>
         <button className='gotoVote' onClick={()=>navigate('/')}>
-          gotovote
+          +
         </button>
       {/*<h1>{communityData.name}</h1>
         <div className="communityDetail" style={{ backgroundImage: `url(${communityData.imagePath})` }}>
