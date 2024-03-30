019d43115da727fd1c8fae6f5e9baf80ee62cf27
jaerius <rylynn1029@naver.com>
2024-03-31 03:19:24 +0900

    기능 구현 성공

--- src/pages/ArtistMainPage/ArtistMainPage.jsx
+++ src/pages/ArtistMainPage/ArtistMainPage.jsx
@@ -1,104 +1,95 @@
-import './ArtistMainPage.css'
-import { GoBack, BottomSidebar, ContentWrapper, LogoAndHead } from '../../Components';
-import {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import React from 'react';
-import Sidebar from '../../Layout/Sidebar';
-import MyLineChart from '../../components/Line';
 import { useLocation } from 'react-router-dom';
-
-
-/*
-const idolsData = [
-    { id: 1, name: "Kim Chewon", agency: "LE SSERAFIM", profileImg: "", vote:2.97 },
-    { id: 2, name: "Park Jimin", agency: "BTS", profileImg: "" },
-    { id: 3, name: "Kang Daniel", agency: "KONNECT Entertainment", profileImg: "" },
-    { id: 4, name: "Lee Chaeyeon", agency: "WM Entertainment", profileImg: "" },
-    { id: 5, name: "Miyawaki Sakura", agency: "HKT48", profileImg: "" },
-    { id: 6, name: "Choi Yena", agency: "Yuehua Entertainment", profileImg: "" },
-    { id: 7, name: "Kwon Eunbi", agency: "Woollim Entertainment", profileImg: "" },
-    { id: 8, name: "Kim Minju", agency: "Urban Works Media", profileImg: "" },
-    { id: 9, name: "Ahn Yujin", agency: "Starship Entertainment", profileImg: "" },
-    { id: 10, name: "Jang Wonyoung", agency: "Starship Entertainment", profileImg: "" }
-  ];
-*/
-
+import './ArtistMainPage.css';
+import Sidebar from '../../Layout/Sidebar'; // 경로에 따라 수정해야 할 수도 있습니다.
+import MyLineChart from '../../components/Line'; // 경로에 따라 수정해야 할 수도 있습니다.
+import { useWallet } from '../../WalletContext'; // 경로에 따라 수정해야 할 수도 있습니다.
+import { ContentWrapper } from '../../Components'; // 경로에 따라 수정해야 할 수도 있습니다.
 
 function ArtistMainPage() {
     const location = useLocation();
     const idolDataFromPreviousPage = location.state?.idol;
+    const { walletAddress } = useWallet();
 
-    const [idolData, setIdolData] = useState({});
+    const [showInput, setShowInput] = useState(false); // 입력 창 표시 여부
+    const [voteValue, setVoteValue] = useState(''); // 투표 값
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
     useEffect(() => {
-        // 아이돌 정보가 존재할 때만 요청을 보냅니다.
         if (idolDataFromPreviousPage?.id) {
             axios.put(`http://localhost:8800/api/idol/idolProfile/${idolDataFromPreviousPage.id}`)
-            .then(response => {
-                setIdolData(response.data);
-            })
-            .catch((err) => {
-                console.error("Failed to get an idol", err);
-            });
+                .then(response => {
+                    // 아이돌 정보 업데이트
+                })
+                .catch((err) => {
+                    console.error("Failed to get an idol", err);
+                });
         }
-    }, [idolDataFromPreviousPage?.id]); 
+    }, [idolDataFromPreviousPage?.id]);
 
+    const handleVoteClick = () => {
+        if (!showInput) {
+            setShowInput(true); // 입력 창을 표시
+        } else {
+            // 이미 입력 창이 표시된 상태이며, 사용자가 값을 입력하고 'Vote'를 다시 누름
+            if (voteValue.trim() === '') {
+                // 입력값이 공백인 경우 입력 창을 닫음
+                setShowInput(false);
+                setVoteValue('');
+            } else if (isNaN(voteValue)) {
+                // 입력값이 숫자가 아닌 경우 경고하고 초기화
+                alert('Please enter a valid number.');
+                setVoteValue('');
+            } else {
+                // 유효한 숫자가 입력된 경우 투표를 진행
+                axios.post('http://localhost:8800/api/vote/vote', {
+                    idolId: idolDataFromPreviousPage.idolId,
+                    walletAddress: walletAddress,
+                    paidTokens: parseFloat(voteValue),
+                }).then(response => {
+                    console.log('Vote successful', response);
+                    setShowInput(false); // 입력 창 숨기기
+                    setVoteValue(''); // 입력 값 초기화
+                    setShowSuccessMessage(true);
 
-    return(
-    <ContentWrapper>
-        <div className="ArtistMainPage">
-            <img className='ArtistMainPageidolImg' src={idolDataFromPreviousPage.profilePic} alt={idolDataFromPreviousPage.name}/>
-               
-            
-         <div className='content-wrapper'>
-            <div className='ArtistMainPageGlass'>
-                <div className='ArtistMainPageidolname'>
-                    {idolDataFromPreviousPage.name}
-                </div>
-                <div className='ArtistMainPageidolagency'>
-                    {idolDataFromPreviousPage.agency}
-                </div>
-                <div className='TotalVotedTitle'>
-                    Total Voted Rappo
-                </div>
-                <div className='TotalVote'>
-                    {idolDataFromPreviousPage.totalTokens}
-                </div>
-                <button className='VoteButton'>
-                    Vote
-                </button>
-            </div>
+                }).catch(error => {
+                    console.error('Failed to submit vote', error);
+                });
+            }
+        }
+    };
 
-           <div className="graphContainer">
-                <MyLineChart/>
-            </div> 
-
-            <div className="ArtistMainPageContent">
-
-                <div className="RappoTitle">Rappo Ranking</div>
-
-                    <div className="RappoRanking">    
-                    
-                    <div key={idolDataFromPreviousPage.idolId} className="Artistcontents1">
-                        
-                        <img src={idolDataFromPreviousPage.profileImg} alt={idolDataFromPreviousPage.name} /> {/* 이 부분에 실제 이미지 태그나 경로를 사용할 수 있습니다. */}
-                        
-                        <div className="info">
-                        <div className="name">
-                            {idolDataFromPreviousPage.name}
-                        </div>
-                        <div className="agency">
-                            {idolDataFromPreviousPage.agency}
-                        </div>
-                        </div>
+    return (
+        <ContentWrapper>
+            <div className="ArtistMainPage">
+                <img className='ArtistMainPageidolImg' src={idolDataFromPreviousPage.profilePic} alt={idolDataFromPreviousPage.name} />
+                <div className='content-wrapper'>
+                    <div className='ArtistMainPageGlass'>
+                        <div className='ArtistMainPageidolname'>{idolDataFromPreviousPage.name}</div>
+                        <div className='ArtistMainPageidolagency'>{idolDataFromPreviousPage.agency}</div>
+                        <div className='TotalVotedTitle'>Total Voted Rappo</div>
+                        <div className='TotalVote'>{idolDataFromPreviousPage.vote}</div>
+                        {showInput && (
+                            <input
+                                type="text"
+                                value={voteValue}
+                                onChange={(e) => setVoteValue(e.target.value)}
+                                placeholder="Enter your vote"
+                                className='VoteInput'
+                            />
+                        )}
+                        <button className='VoteButton' onClick={handleVoteClick}>
+                            {showInput ? 'Submit' : 'Vote'}
+                        </button>
+                        {showSuccessMessage && <p>success!</p>}
                     </div>
-                    
-                    </div>
-            </div>
+                <div className="graphContainer">
+                    <MyLineChart />
+                </div> 
             </div>
         </div>
-        
-        <Sidebar/>
+        <Sidebar />
     </ContentWrapper>
     )
 }
